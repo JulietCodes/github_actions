b5316149b77ac7d2aeee7809a77b6869693dec89
Juliet <kinuthiajuliet@gmail.com>
2024-01-31 10:21:09 +0000

    update file_test.py according to pep8

--- .github/workflows/file_test.py
+++ .github/workflows/file_test.py
@@ -2,6 +2,26 @@ import pytest
 
 def tet_calc_addition():
   # Function test the output of 2+4
+  output = 2+ 4
+  assert output ==6
+
+def test_calc_subtraction():
+  # Function to test the output 2-4
+  output = 2-4
+  assert output == -2
+
+def test_calc_multiply():
+  # Function test the output 2*4
+  output = 2 * 4
+  assert output == 8
+
+def test_calc_coucou():
+  # Function test if the output returns 'hello'
+  output = 'hello'
+  assert output == 'hello' import pytest
+
+def tet_calc_addition():
+  # Function test the output of 2+4
     output = 2+ 4
     assert output ==6
 
